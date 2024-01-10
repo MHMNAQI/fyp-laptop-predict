56564dad8617327a33a249085197db79df6c7d2d
naqiuddin <naqiuddin847@gmail.com>
2024-01-10 18:51:44 +0800

    initial commit

---
+++ import joblib.py
@@ -0,0 +1,5 @@
+import joblib
+
+loaded_data = joblib.load('qidin_poke.joblib')
+
+print(loaded_data)
---
+++ run.py
@@ -0,0 +1,89 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from flask_migrate import Migrate
+from sys import exit
+from decouple import config
+
+from apps.config import config_dict
+from apps import create_app, db
+
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# WARNING: Don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=True, cast=bool)
+
+# The configuration
+get_config_mode = 'Debug' if DEBUG else 'Production'
+
+try:
+
+    # Load the configuration using the default values
+    app_config = config_dict[get_config_mode.capitalize()]
+
+except KeyError:
+    exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
+
+app = create_app(app_config)
+Migrate(app, db)
+
+if DEBUG:
+    app.logger.info('DEBUG       = ' + str(DEBUG))
+    app.logger.info('Environment = ' + get_config_mode)
+    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
+
+if __name__ == "__main__":
+    app.run()
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/price-system', methods=['POST', 'GET'])
+def index():
+
+    pred_value = 0
+    if request.method == 'POST':
+        ram = request.form['ram']
+        weight = request.form['weight']
+        company = request.form['company']
+        typename = request.form['typename']
+        opsys = request.form['opsys']
+        cpu = request.form['cpuname']
+        gpu = request.form['gpuname']
+        touchscreen = request.form.getlist('touchscreen')
+        ips = request.form.getlist('ips')
+        
+        feature_list = []
+        
+        feature_list.append(int(ram))
+        feature_list.append(float(weight))
+        feature_list.append(len(touchscreen))
+        feature_list.append(len(ips))
+
+        company_list = ['acer','apple','asus','dell','hp','lenovo','msi','other','toshiba']
+        typename_list = ['2in1convertible','gaming','netbook','notebook','ultrabook','workstation']
+        opsys_list = ['linux','mac','other','windows']
+        cpu_list = ['amd','intelcorei3','intelcorei5','intelcorei7','other']
+        gpu_list = ['amd','intel','nvidia']
+
+        def traverse_list(lst, value):
+            for item in lst:
+                if item == value:
+                    feature_list.append(1)
+                else:
+                    feature_list.append(0) 
+
+        traverse_list(company_list, company)
+        traverse_list(typename_list, typename)
+        traverse_list(opsys_list, opsys)
+        traverse_list(cpu_list, cpu)
+        traverse_list(gpu_list, gpu)
+
+        feature_list = [feature_list]
+
+        pred_value = model.predict(feature_list)
+
+    return render_template('price-system.html', pred_value=pred_value)
